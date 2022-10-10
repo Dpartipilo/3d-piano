df87312bf1be7c1925db80000b78055b1d43e453
Dpartipilo <Dpartipilo@hotmail.com>
2022-10-10 23:00:11 +0100

    Refactored WhiteKey into a generic KeyboardKey

---
+++ src/components/KeyboardKey.tsx
@@ -0,0 +1,92 @@
+import * as THREE from "three";
+import React, { useEffect, useRef, useState } from "react";
+import { ThreeEvent, useFrame } from "@react-three/fiber";
+import { playAudio } from "../utils";
+import { useKeyboardControls } from "@react-three/drei";
+
+type KeyboardKeyProps = {
+  musicNote: string;
+  position?: any;
+  isBlackKey: boolean;
+  name: string;
+};
+
+export const KeyboardKey = (props: KeyboardKeyProps) => {
+  const [playing, setPlaying] = useState(false);
+  const { musicNote, isBlackKey, name } = props;
+  const pressed = useKeyboardControls((state) => state[name]);
+
+  console.log(pressed);
+
+  // useFrame(() => {
+  //   // Fetch fresh data from store
+  //   const pressed = get().backward;
+  //   console.log(pressed);
+  // });
+  const meshRef = useRef<THREE.Mesh>(null!);
+
+  // const [sub, get] = useKeyboardControls();
+
+  // useEffect(() => {
+  //   return sub(
+  //     (state: any) => state.C4,
+  //     (pressed: any) => {
+  //       console.log("C4", pressed);
+  //     }
+  //   );
+  // }, []);
+
+  useEffect(() => {
+    const key = meshRef.current;
+    if (pressed || playing) {
+      key.rotation.x = 0.1;
+      playAudio(musicNote);
+    } else {
+      key.rotation.x = 0;
+    }
+  }, [pressed, playing, musicNote]);
+
+  // useEffect(() => {
+  //   const key = meshRef.current;
+  //   if (playing) {
+  //     key.rotation.x = 0.1;
+  //   } else {
+  //     key.rotation.x = 0;
+  //   }
+  // }, [playing]);
+
+  const handleOnPoinerDown = (e: ThreeEvent<MouseEvent>) => {
+    e.stopPropagation();
+    setPlaying(true);
+    // playAudio(musicNote);
+  };
+
+  const handleOnPoinerUp = (e: ThreeEvent<MouseEvent>) => {
+    e.stopPropagation();
+    setPlaying(false);
+  };
+
+  const handleOnPoinerOut = (e: ThreeEvent<MouseEvent>) => {
+    e.stopPropagation();
+    setPlaying(false);
+  };
+
+  return (
+    <mesh
+      {...props}
+      ref={meshRef}
+      onPointerDown={(e) => {
+        handleOnPoinerDown(e);
+      }}
+      onPointerUp={(e) => {
+        handleOnPoinerUp(e);
+      }}
+      onPointerOut={(e) => {
+        handleOnPoinerOut(e);
+      }}
+    >
+      <boxGeometry args={isBlackKey ? [1, 1.9, 5] : [1, 1, 8]}></boxGeometry>
+      <meshStandardMaterial color={isBlackKey ? "black" : "white"} />
+    </mesh>
+  );
+};
