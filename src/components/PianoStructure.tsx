55b98c3344dfd0664a910c444c88308851e57892
Dpartipilo <Dpartipilo@hotmail.com>
2022-11-07 10:59:46 +0000

    Refactored PianoStructure component

--- src/components/PianoStructure.tsx
+++ src/components/PianoStructure.tsx
@@ -1,52 +1,86 @@
 import React from "react";
-import { GroupProps } from "@react-three/fiber";
+import {
+  GroupProps,
+  MeshProps,
+  BoxGeometryProps,
+  MeshPhysicalMaterialProps,
+  Color,
+} from "@react-three/fiber";
 import { PowerLight } from "./PowerLight";
-import { FormidableLogo } from "./FormidableLogo";
+import { FormidableLogo } from "./Logo";
 
 type PianoStructureProps = GroupProps & {
   power: boolean;
+  pianoColor?: Color;
 };
 
+type PianoBlockProps = {
+  mesh?: MeshProps;
+  material?: MeshPhysicalMaterialProps;
+  geometry?: BoxGeometryProps;
+  children?: any;
+};
+
+const PianoBlock = ({
+  mesh,
+  geometry,
+  material,
+  children,
+}: PianoBlockProps) => (
+  <mesh {...mesh}>
+    <boxGeometry {...geometry} />
+    <meshPhysicalMaterial {...material} />
+    {children}
+  </mesh>
+);
+
 export const PianoStructure = (props: PianoStructureProps) => {
-  const { power } = props;
+  const { power, pianoColor } = props;
 
   return (
     <group {...props} name="Piano body">
-      <mesh castShadow position={[2.8, 4.75, -4.1]} name="Back piece">
+      <PianoBlock
+        mesh={{
+          castShadow: true,
+          position: [2.8, 4.75, -4.1],
+          name: "Back piece",
+        }}
+        geometry={{ args: [27.3, 2.5, 2] }}
+        material={{ color: pianoColor, roughness: 0, reflectivity: 0.8 }}
+      >
         <FormidableLogo />
+      </PianoBlock>
 
-        <boxGeometry args={[27.3, 2.5, 2]} />
-        <meshPhysicalMaterial
-          color={"#1f1f1e"}
-          roughness={0}
-          reflectivity={0.8}
-        />
-      </mesh>
-      <mesh castShadow position={[-10.1, 4.5, -0.25]} name="Left side">
-        <boxGeometry args={[1.5, 2, 9.5]} />
-        <meshPhysicalMaterial
-          color={"#1f1f1e"}
-          roughness={0}
-          reflectivity={0.8}
-        />
-      </mesh>
-      <mesh castShadow position={[15.7, 4.5, -0.25]} name="Right side">
-        <boxGeometry args={[1.5, 2, 9.5]} />
-        <meshPhysicalMaterial
-          color={"#1f1f1e"}
-          roughness={0}
-          reflectivity={0.8}
-        />
-      </mesh>
-      <mesh castShadow position={[2.8, 3.5, -0.3]} name="Bottom side">
-        <boxGeometry args={[27.3, 0.7, 9.6]} />
-        <meshPhysicalMaterial
-          color={"#1f1f1e"}
-          opacity={1}
-          roughness={0}
-          reflectivity={1}
-        />
-      </mesh>
+      <PianoBlock
+        mesh={{
+          castShadow: true,
+          position: [-10.1, 4.5, -0.25],
+          name: "Left side",
+        }}
+        geometry={{ args: [1.5, 2, 9.5] }}
+        material={{ color: pianoColor, roughness: 0, reflectivity: 0.8 }}
+      />
+
+      <PianoBlock
+        mesh={{
+          castShadow: true,
+          position: [15.7, 4.5, -0.25],
+          name: "Right side",
+        }}
+        geometry={{ args: [1.5, 2, 9.5] }}
+        material={{ color: pianoColor, roughness: 0, reflectivity: 0.8 }}
+      />
+
+      <PianoBlock
+        mesh={{
+          castShadow: true,
+          position: [2.8, 3.5, -0.3],
+          name: "Right side",
+        }}
+        geometry={{ args: [27.3, 0.7, 9.6] }}
+        material={{ color: pianoColor, roughness: 0, reflectivity: 0.8 }}
+      />
+
       <PowerLight power={power} />
       {props.children}
     </group>
