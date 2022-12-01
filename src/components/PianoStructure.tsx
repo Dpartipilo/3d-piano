4388720fa3232320f9eb50444afc6808099cb874
Dpartipilo <Dpartipilo@hotmail.com>
2022-12-01 22:24:56 +0000

    Refactored  PianoStructure component & added LevelDials

--- src/components/PianoStructure.tsx
+++ src/components/PianoStructure.tsx
@@ -1,19 +1,24 @@
-import { Dispatch, ReactElement, SetStateAction } from "react";
+import { ReactElement, useContext, useState } from "react";
+import * as THREE from "three";
 import {
   GroupProps,
   MeshProps,
   BoxGeometryProps,
   MeshPhysicalMaterialProps,
   Color,
+  useFrame,
 } from "@react-three/fiber";
 import { Logo } from "./Logo";
 import { PowerButton } from "./PowerButton";
+import { LevelDial } from "./LevelDial/LevelDial";
+import { DialsArea } from "./LevelDial";
+import { ColorRepresentation } from "three";
+import { PianoContext } from "./PianoContext";
 
 type PianoStructureProps = GroupProps & {
   size: number;
-  power: boolean;
-  setPower: Dispatch<SetStateAction<boolean>>;
   pianoColor?: Color;
+  lightColor?: ColorRepresentation;
 };
 
 type PianoBlockProps = {
@@ -29,7 +34,12 @@ const PianoBlock = ({
   material,
   children,
 }: PianoBlockProps) => (
-  <mesh {...mesh}>
+  <mesh
+    {...mesh}
+    onPointerDown={(e) => {
+      e.stopPropagation();
+    }}
+  >
     <boxGeometry {...geometry} />
     <meshPhysicalMaterial {...material} />
     {children}
@@ -37,7 +47,36 @@ const PianoBlock = ({
 );
 
 export const PianoStructure = (props: PianoStructureProps) => {
-  const { power, pianoColor, size, setPower } = props;
+  const { pianoColor, lightColor = "red", size } = props;
+  const [onControls, setOnControls] = useState(false);
+  const {
+    setSoundGain,
+    setSoundAttack,
+    setSoundDecay,
+    setSoundSustain,
+    setSoundRealease,
+  } = useContext(PianoContext);
+
+  useFrame(({ camera, mouse }) => {
+    if (onControls) {
+      camera.position.x = THREE.MathUtils.lerp(camera.position.x, 5, 0.09);
+      camera.position.y = THREE.MathUtils.lerp(camera.position.y, 16, 0.09);
+      camera.position.z = THREE.MathUtils.lerp(camera.position.z, 3, 0.09);
+      camera.lookAt(5, 0, 0);
+    } else {
+      camera.rotation.x = THREE.MathUtils.lerp(
+        camera.rotation.x,
+        mouse.y / 2,
+        0.5
+      );
+      camera.position.x = THREE.MathUtils.lerp(camera.position.x, mouse.x, 0.2);
+      camera.position.y = THREE.MathUtils.lerp(53, mouse.y * 1, 0.5);
+      camera.position.z = THREE.MathUtils.lerp(40, mouse.y * 18, 0.6);
+      camera.lookAt(0, 0, 0);
+    }
+
+    return null;
+  });
 
   return (
     <group {...props} name="Piano body">
@@ -48,7 +87,7 @@ export const PianoStructure = (props: PianoStructureProps) => {
           name: "Back piece",
         }}
         geometry={{ args: [size, 2.5, 2] }}
-        material={{ color: pianoColor, roughness: 0, reflectivity: 0.8 }}
+        material={{ color: pianoColor, roughness: 0.4, reflectivity: 0.3 }}
       />
 
       <PianoBlock
@@ -58,7 +97,7 @@ export const PianoStructure = (props: PianoStructureProps) => {
           name: "Left side",
         }}
         geometry={{ args: [1.5, 2, 9.5] }}
-        material={{ color: pianoColor, roughness: 0, reflectivity: 0.8 }}
+        material={{ color: pianoColor, roughness: 0.4, reflectivity: 0.3 }}
       />
 
       <PianoBlock
@@ -68,7 +107,7 @@ export const PianoStructure = (props: PianoStructureProps) => {
           name: "Right side",
         }}
         geometry={{ args: [1.5, 2, 9.5] }}
-        material={{ color: pianoColor, roughness: 0, reflectivity: 0.8 }}
+        material={{ color: pianoColor, roughness: 0.4, reflectivity: 0.3 }}
       />
 
       <PianoBlock
@@ -82,11 +121,63 @@ export const PianoStructure = (props: PianoStructureProps) => {
       />
       <group position={[0, 6, 0]} name="Top Piano Toolbar">
         <Logo position={[0.45, 0.04, -0.5]} />
-        <PowerButton
-          setPower={setPower}
-          power={power}
-          position={[size - 1.5, 0, -0.2]}
-        />
+        <PowerButton position={[size - 1.5, 0, -0.2]} />
+        <group
+          onClick={() => setOnControls(true)}
+          onPointerMissed={() => setOnControls(false)}
+        >
+          <DialsArea position={[12, 0.01, 1.8]} />
+
+          <LevelDial
+            lightColor={lightColor}
+            position={[14, 0.2, 0]}
+            maxValue={2}
+            minValue={0}
+            step={0.1}
+            label="Attack"
+            setValue={setSoundAttack}
+          />
+
+          <LevelDial
+            lightColor={lightColor}
+            position={[15.5, 0.2, 0]}
+            maxValue={2}
+            minValue={0}
+            step={0.1}
+            label="Decay"
+            setValue={setSoundDecay}
+          />
+
+          <LevelDial
+            lightColor={lightColor}
+            position={[17, 0.2, 0]}
+            maxValue={2}
+            minValue={0}
+            step={0.1}
+            label="Sustain"
+            setValue={setSoundSustain}
+          />
+
+          <LevelDial
+            lightColor={lightColor}
+            position={[18.5, 0.2, 0]}
+            maxValue={5}
+            minValue={0}
+            step={0.1}
+            label="Release"
+            setValue={setSoundRealease}
+          />
+
+          <LevelDial
+            lightColor={lightColor}
+            position={[21, 0.2, 0]}
+            maxValue={5}
+            minValue={0}
+            step={0.1}
+            label="Gain"
+            setValue={setSoundGain}
+          />
+        </group>
       </group>
     </group>
   );
