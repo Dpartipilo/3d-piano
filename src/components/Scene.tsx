0619cb09545228ae9254b0313910420f6b132168
Dpartipilo <Dpartipilo@hotmail.com>
2022-12-02 14:13:48 +0000

    Removed cursor light and Light leva controls

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -9,41 +9,50 @@ import { useControls } from "leva";
 export const Scene = (props: any) => {
   const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
-  const { x, y, z, angle, intensity, distance, penumbra, color } = useControls(
-    "Light properties",
-    {
-      x: { value: 0.5, min: -15, max: 18, step: 0.1 },
-      y: { value: 10, min: 0, max: 15, step: 0.1 },
-      z: { value: 0, min: -10, max: 10, step: 0.1 },
-      angle: { value: 0.6, min: 0, max: 1, step: 0.01 },
-      intensity: { value: 0.8, min: 0, max: 3, step: 0.01 },
-      distance: { value: 18, min: 0, max: 20, step: 0.01 },
-      penumbra: { value: 0.5, min: 0, max: 1, step: 0.01 },
-      color: "#f5eede",
-    }
-  );
+  // const { x, y, z, angle, intensity, distance, penumbra, color } = useControls(
+  //   "Light properties",
+  //   {
+  //     x: { value: 0.5, min: -15, max: 18, step: 0.1 },
+  //     y: { value: 10, min: 0, max: 15, step: 0.1 },
+  //     z: { value: 0, min: -10, max: 10, step: 0.1 },
+  //     angle: { value: 0.6, min: 0, max: 1, step: 0.01 },
+  //     intensity: { value: 0.8, min: 0, max: 3, step: 0.01 },
+  //     distance: { value: 18, min: 0, max: 20, step: 0.01 },
+  //     penumbra: { value: 0.5, min: 0, max: 1, step: 0.01 },
+  //     color: "#f5eede",
+  //   }
+  // );
+
+  const intensity = 0.8;
+  const distance = 18;
+  const penumbra = 0.7;
+  const angle = 1;
+  const x = 0.5;
+  const y = 10;
+  const z = -2;
+  const color = "#f5eede";
 
   // Light on pointer
-  useFrame(({ mouse }) => {
-    if (isMobile) return;
+  // useFrame(({ mouse }) => {
+  //   if (isMobile) return;
 
-    if (cursorLightRef.current) {
-      cursorLightRef.current.position.x = mouse.x * 16;
-      cursorLightRef.current.position.z = -mouse.y * 10;
-    }
+  //   if (cursorLightRef.current) {
+  //     cursorLightRef.current.position.x = mouse.x * 16;
+  //     cursorLightRef.current.position.z = -mouse.y * 10;
+  //   }
 
-    return null;
-  });
+  //   return null;
+  // });
 
   const spotlightRef = useRef<any>();
-  const cursorLightRef = useRef<THREE.PointLight>(null);
+  // const cursorLightRef = useRef<THREE.PointLight>(null);
   return (
     <>
       {/********** Lights ************/}
       <ambientLight args={[0xffffff, 0.4]} />
-      <hemisphereLight args={[color, 0x080820, 0.3]} />
+      <hemisphereLight args={[color, 0x080820, 0.4]} />
 
-      {isMobile ? null : (
+      {/* {isMobile ? null : (
         <pointLight
           ref={cursorLightRef}
           args={["#70a0d4", 6, 15, 4]}
@@ -54,7 +63,7 @@ export const Scene = (props: any) => {
             args={[-10, 10, 10, -10]}
           />
         </pointLight>
-      )}
+      )} */}
 
       <spotLight
         ref={spotlightRef}
